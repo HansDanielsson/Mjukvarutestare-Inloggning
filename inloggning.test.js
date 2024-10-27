c6f795097f3bc15d64484f98cf803b5a249c247b
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2024-10-27 17:14:49 +0100

    Save changes

--- inloggning.test.js
+++ inloggning.test.js
@@ -2,6 +2,8 @@ const Inloggning = require('./inloggning.js')
 
 describe('Inloggning', () => {
   let myInloggning
+  let username
+  let password
 
   // Before All
   beforeAll(() => {
@@ -10,19 +12,18 @@ describe('Inloggning', () => {
 
   // Before each
   beforeEach(() => {
-    myInloggning.username = 'username'
-    myInloggning.password = '1Password'
+    username = 'username'
+    password = '1Password'
+    myInloggning.username = username
+    myInloggning.password = password
   })
 
   it('Kolla upp korrekt username och password, return true', () => {
-    const username = 'username'
-    const password = '1Password'
     const result = myInloggning.checkUsername(username, password)
     expect(result).toBeTruthy()
   })
 
   it('Kolla upp username och fel password, return error text', () => {
-    const username = 'username'
     const password = 'wrong'
     const result = myInloggning.checkUsername(username, password)
     expect(result).toBe('Wrong username or password')
@@ -30,7 +31,6 @@ describe('Inloggning', () => {
 
   it('Kolla upp fel username och password, return error text', () => {
     const username = 'wrong'
-    const password = '1Password'
     const result = myInloggning.checkUsername(username, password)
     expect(result).toBe('Wrong username or password')
   })
@@ -44,13 +44,11 @@ describe('Inloggning', () => {
 
   it('Kolla upp tom username och korrekt password, return error text', () => {
     let username
-    const password = '1Password'
     const result = myInloggning.checkUsername(username, password)
     expect(result).toBe('Wrong username or password')
   })
 
   it('Kolla upp korrekt username och tom password, return error text', () => {
-    const username = 'username'
     let password
     const result = myInloggning.checkUsername(username, password)
     expect(result).toBe('Wrong username or password')
@@ -66,6 +64,8 @@ describe('Inloggning', () => {
 
 describe('Skapa ny user', () => {
   let myInloggning
+  let username
+  let password
 
   // Before all
   beforeAll(() => {
@@ -74,105 +74,101 @@ describe('Skapa ny user', () => {
 
   // Before each
   beforeEach(() => {
+    username = 'NewUsername'
+    password = '1Password'
     myInloggning.username = 'username'
-    myInloggning.password = '1Password'
+    myInloggning.password = password
   })
 
   it('Skapa ny user, return username, password', () => {
-    const username = 'New username'
-    const password = '1Password'
+    password = '1NewPassword'
     const result = myInloggning.createUser(username, password)
     expect(result.username).toBe(username)
     expect(result.password).toBe(password)
   })
 
   it('Skapa ny user med befintligt username, return error text', () => {
-    const username = 'username'
-    const password = '1Password'
+    username = 'username'
     const result = myInloggning.createUser(username, password)
     expect(result).toBe('Username already exists')
   })
 
   it('Skapa ny user med under 8 tecken password, return error text', () => {
-    const username = 'New username'
-    const password = '1Passwo'
+    password = '1Passwo'
     const result = myInloggning.createUser(username, password)
     expect(result).toBe('Password does not meet the security requirements')
   })
 
   it('Skapa ny user fel password A-Z, return error text', () => {
-    const username = 'New username'
-    const password = '1password'
+    password = '1password'
     const result = myInloggning.createUser(username, password)
     expect(result).toBe('Password does not meet the security requirements')
   })
 
   it('Skapa ny user fel password a-z, return error text', () => {
-    const username = 'New username'
-    const password = '1PASSWORD'
+    password = '1PASSWORD'
     const result = myInloggning.createUser(username, password)
     expect(result).toBe('Password does not meet the security requirements')
   })
 
   it('Skapa ny user fel password 0-9, return error text', () => {
-    const username = 'New username'
-    const password = 'Password'
+    password = 'Password'
     const result = myInloggning.createUser(username, password)
     expect(result).toBe('Password does not meet the security requirements')
   })
 })
 
 describe('Ändra lösenordet på användare', () => {
-  // let myInloggning
+  let myInloggning
+  let user
+  let oldPassword
+  let newPassword
 
   // Before all
   beforeAll(() => {
-    myInloggning = new Inloggning('username', '1Password')
+    user = 'username'
+    myInloggning = new Inloggning(user, '1Password')
   })
 
   // Before each
   beforeEach(() => {
-    myInloggning.username = 'username'
-    myInloggning.password = '1Password'
+    oldPassword = '1Password'
+    newPassword = '1NewPassword'
+    myInloggning.username = user
+    myInloggning.password = oldPassword
   })
 
   it('Ändra på en användare som finns, return true', () => {
-    const user = 'username'
-    const oldPassword = '1Password'
-    const newPassword = '1NewPassword'
     const result = myInloggning.changePassword(user, oldPassword, newPassword)
     expect(result).toBeTruthy()
+    expect(myInloggning.password).toBe(newPassword)
   })
 
   it('Ändra på en användare som inte finns, return error text', () => {
-    const user = 'Not a user'
-    const oldPassword = '1Password'
-    const newPassword = '1NewPassword'
+    user = 'NotaUser'
     const result = myInloggning.changePassword(user, oldPassword, newPassword)
     expect(result).toBe('Wrong username')
+    expect(myInloggning.password).toBe('1Password')
   })
 
   it('Ändra på en användare som har fel password, return error text', () => {
-    const user = 'username'
-    const oldPassword = '1OldPassword'
-    const newPassword = '1NewPassword'
+    oldPassword = '1OldPassword'
     const result = myInloggning.changePassword(user, oldPassword, newPassword)
     expect(result).toBe('Wrong password')
+    expect(myInloggning.password).toBe('1Password')
   })
 
   it('Ändra på en användare som har samma password, return error text', () => {
-    const user = 'username'
-    const oldPassword = '1Password'
-    const newPassword = '1Password'
+    newPassword = '1Password'
     const result = myInloggning.changePassword(user, oldPassword, newPassword)
     expect(result).toBe('New password must be different from old password')
+    expect(myInloggning.password).toBe('1Password')
   })
 
   it('Ändra på en användare som har nya password securiy req, return error text', () => {
-    const user = 'username'
-    const oldPassword = '1Password'
-    const newPassword = '1password'
+    newPassword = '1password'
     const result = myInloggning.changePassword(user, oldPassword, newPassword)
     expect(result).toBe('New password does not meet the security requirements')
+    expect(myInloggning.password).toBe('1Password')
   })
 })
